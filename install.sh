dd80103babb995461842cd86fbea9eae7c60dc6a
Christoph Boecklin <christoph.boecklin@swisstopo.ch>
2018-12-18 08:53:09 +0100

    add a dedicated bash_env file

--- install.sh
+++ install.sh
@@ -14,3 +14,4 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
 graceful_ln ${DIR}/bash_aliases 	~/.bash_aliases
 graceful_ln ${DIR}/vimrc 			~/.vimrc
 graceful_ln ${DIR}/profile          ~/.profile
+graceful_ln ${DIR}/bash_env         ~/.bash_env
